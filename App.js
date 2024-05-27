c303a43c6ff5858f952a76ceb9fa80f7e5ec743b
amasroeda <amasroeda@gmail.com>
2024-05-27 15:03:48 +0000

    modifications

--- App.js
+++ App.js
@@ -1,1 +1,25 @@
-ECHO is on.
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        My name is <Text style={styles.bold}>Ama</Text>
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#99b2cc',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+});
